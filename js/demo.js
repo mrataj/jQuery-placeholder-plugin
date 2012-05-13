08be032e9a2c4f034312eded5815b951aefa7a98
Miha Rataj <rataj.miha@gmail.com>
2012-05-13 18:32:12 +0200

    initial commit

---
+++ js/demo.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function() {
+	// Init placeholder inputs
+	$('#inputName').placeholder({
+		text: 'Enter your name ...'
+	});
+	
+	$('#inputSurname').placeholder({
+		text: 'Enter your surname ...'
+	});
+	
+	$('#inputAge').placeholder({
+		text: 'Your age ...'
+	});
+
+	// Init button
+});
---
+++ js/jquery.placeholder.js
@@ -0,0 +1,64 @@
+﻿jQuery.fn.placeholder = function (options) {
+  var defaults = {
+    text: '',
+    css: '',
+    top: 0,
+    left: 0,
+    color: '#000',
+    fontsize: '1.0em'
+  };
+
+  var settings = $.extend({}, defaults, options);
+
+  return this.each(function () {
+
+    var input = $(this);
+
+    var placeholder = $('<div></div>');
+    placeholder.addClass(settings.css);
+    placeholder.text(settings.text);
+    placeholder.css('position', 'absolute');
+    placeholder.css('top', input.position().top + settings.top);
+    placeholder.css('left', input.position().left + settings.left);
+    placeholder.css('color', settings.color);
+    placeholder.css('opacity', 0.7);
+    placeholder.css('cursor', input.css('cursor'));
+    placeholder.css('font-size', settings.fontsize);
+
+    var parent = input.parent();
+    parent.append(placeholder);
+
+    toggle();
+
+    function toggle() {
+      var haveFocus = input.is(":focus");
+      var empty = (input.val().length == 0);
+
+      if (haveFocus)
+        placeholder.hide();
+
+      if (!haveFocus && empty)
+        placeholder.show();
+    }
+
+    $(input).bind('change', toggle);
+
+    // When input got focus, hide placeholder.
+    $(input).bind('focusin', function () {
+      placeholder.hide();
+    });
+
+    // When we lose focus and input is empty, show placeholder.
+    $(input).bind('focusout', function () {
+      var empty = (input.val().length == 0);
+      if (empty)
+        placeholder.show();
+    });
+
+    // Treat placeholder click as click on input.
+    $(placeholder).bind('click', function () {
+      input.focus();
+    });
+
+  });
+};
